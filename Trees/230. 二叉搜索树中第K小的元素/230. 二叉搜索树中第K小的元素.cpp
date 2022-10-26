d2537fe1c41b89b85129c5a9d2bb3c6d95529df4
SamisAlive <sam5968947@yahoo.com.tw>
2022-10-26 12:50:43 +0800

    first commit

--- Trees/230. 二叉搜索树中第K小的元素/230. 二叉搜索树中第K小的元素.cpp
+++ Trees/230. 二叉搜索树中第K小的元素/230. 二叉搜索树中第K小的元素.cpp
@@ -9,6 +9,8 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// preorder中序解法，差解
 class Solution {
 public:
     int kthSmallest(TreeNode* root, int k) {
@@ -34,5 +36,29 @@ public:
         sort(all_v.begin(), all_v.begin() + all_v.size());
 
         return all_v[k - 1];
+    }
+};
+
+// inorder中序解法，較佳
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        vector<int> res;
+        if (root == nullptr) return 0;
+        stack<TreeNode *> s;
+
+        while (root != nullptr || !s.empty()) {
+            while (root != nullptr) {
+            s.push(root);
+            root = root->left;
+            } 
+
+            TreeNode *node = s.top();
+            s.pop();
+            res.push_back(node->val);
+            root = node->right;
+        }
+
+        return res[k - 1];
     }
 };
--- Trees/230. 二叉搜索树中第K小的元素/230. 二叉搜索树中第K小的元素.py
+++ Trees/230. 二叉搜索树中第K小的元素/230. 二叉搜索树中第K小的元素.py
@@ -4,6 +4,8 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# preorder中序解法，差解
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         res = []
@@ -23,3 +25,30 @@ class Solution:
         res.sort()
 
         return res[k-1]
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# inorder中序解法，較佳
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        res = []
+        stack = []
+        while root or stack:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+
+            res.append(root.val)
+            # k -= 1
+            # if k == 0:
+            #     return root.val
+
+            root = root.right
+        
+        return res[k - 1]
