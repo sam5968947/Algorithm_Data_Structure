75622790a9b1ebe84eb2145c8af60e4ee1a04873
SamisAlive <sam5968947@yahoo.com.tw>
2022-11-15 20:28:16 +0800

    first commit

--- Two pointer/76. 最小覆盖子串/76. 最小覆盖子串.cpp
+++ Two pointer/76. 最小覆盖子串/76. 最小覆盖子串.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
+        // 求最小window，左while，右for，透過左右指針與s.substr(l_p,r_p)取解
+        unordered_map<char, int> debt;
 
+        for(int i = 0; i < t.length(); i++){
+            debt[t[i]]++;
+        }
+
+        // 計數器，若debt[] = 0時+1，若counter = temp時可能為解
+        int counter = 0;
+        int temp = debt.size();
+
+        // 左指針
+        int l_p = 0;
+
+        for(int r_p = 0; r_p < s.length(); r_p++){
+            char ch = s[r_p];
+            cout<< ch << endl;
+
+
+
+        }
+
+
+
+
+        return "aaa";
     }
 };
