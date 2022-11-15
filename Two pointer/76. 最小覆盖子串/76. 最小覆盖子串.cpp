9c6637e85727df836049fb341ba2635b6ab75e3f
SamisAlive <sam5968947@yahoo.com.tw>
2022-11-15 21:13:00 +0800

    not solve

--- Two pointer/76. 最小覆盖子串/76. 最小覆盖子串.cpp
+++ Two pointer/76. 最小覆盖子串/76. 最小覆盖子串.cpp
@@ -15,17 +15,56 @@ public:
         // 左指針
         int l_p = 0;
 
+        // 解答
+        int min_len = INT_MAX;
+        string min_s = "";
+
+        // 右指針
         for(int r_p = 0; r_p < s.length(); r_p++){
             char ch = s[r_p];
-            cout<< ch << endl;
+            r_p++;
+            // 若ch非我們在意的字元，skip
+            for (int i = 0; i < debt.size(); i++) {
+                if(debt.find(ch) == debt.end()){
+                    continue;
+                }
+            }
+            // ch為我們在意的字元
+            debt[ch]--;
+            if(debt[ch] == 0){
+                counter++;
+            }
+            // 可能為解，移動左指針
+            while(counter == debt.size() && l_p <= r_p){
+                cout<<l_p<<r_p<<endl;
+                // 更新解答
+                int cur_len = r_p - l_p;
+                if(cur_len < min_len){
+                    min_len =  cur_len;
+                    min_s = s.substr(l_p, min_len);
+                    cout<<min_s<<endl;
+                }
 
-
+                char ch = s[l_p];
+                l_p++;
+                // 若ch非我們在意的字元，skip
+                for (int i = 0; i < debt.size(); i++) {
+                    if(debt.find(ch) == debt.end()){
+                        continue;
+                    }
+                }
+                // ch為我們在意的字元
+                debt[ch]++;
+                if(debt[ch] == 1){
+                    counter--;
+                }            
+            }
 
         }
 
-
-
-
-        return "aaa";
+        if(cur_len == INT_MAX){
+            return "";
+        }
+        return min_s;
     }
 };
