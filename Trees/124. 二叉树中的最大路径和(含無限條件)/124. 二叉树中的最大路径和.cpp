1d37f097bf9018478e5b4f5e518bcb2f4afe6286
SamisAlive <sam5968947@yahoo.com.tw>
2022-11-25 13:54:18 +0800

    second practice

--- Trees/124. 二叉树中的最大路径和(含無限條件)/124. 二叉树中的最大路径和.cpp
+++ Trees/124. 二叉树中的最大路径和(含無限條件)/124. 二叉树中的最大路径和.cpp
@@ -10,31 +10,29 @@
  * };
  */
 class Solution {
-
 // C++ class內全域變數宣告方式，等同於python def __init__(self):
 private:
-    // 若節點為單一節點必須走-->ex: -3
     int res = INT_MIN;
 
 private:
-    int dfs(TreeNode* root) {
-        // 中止條件
+    int dfs(TreeNode* root){
+        // 停止條件(優先考慮)
         if(root == nullptr) return 0;
 
         // 取左右節點最大值，若存在負數則為0(不走)
         int l_max = max(dfs(root->left), 0);
         int r_max = max(dfs(root->right), 0);
 
-        // 更新res
+        // 更新解答
         res = max(res, root->val + l_max + r_max);
 
+        // 如case1，若上面還有一個節點5，則1節點(為5的left，且無5的right)return值為1+3，最終解答會變為5+4
         return root->val + max(l_max, r_max);
-    }
+    }    
 
 public:
     int maxPathSum(TreeNode* root) {
         dfs(root);
         return res;
     }
-
 };
