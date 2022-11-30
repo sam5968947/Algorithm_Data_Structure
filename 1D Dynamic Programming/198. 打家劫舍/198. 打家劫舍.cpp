5ad68298da7c21e9b9e1ba29d3e85b0702f257ce
SamisAlive <sam5968947@yahoo.com.tw>
2022-11-30 17:07:39 +0800

    first commit

---
+++ 1D Dynamic Programming/198. 打家劫舍/198. 打家劫舍.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int l = nums.size();
+        if(l == 0) return 0;
+        if(l == 1) return nums[0];
+
+        vector<int> rob(l,0);
+        rob[0] = nums[0];
+        rob[1] = max(nums[0], nums[1]);
+
+        for(int i = 2; i < l; i++){
+            rob[i] = max(rob[i-1], rob[i-2] + nums[i]);
+        }
+
+        return rob[l-1];
+    }
+};
