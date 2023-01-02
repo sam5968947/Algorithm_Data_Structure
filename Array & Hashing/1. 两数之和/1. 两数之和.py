9edea7f67aa840faba3d3d2592a41d8758bf4384
SamisAlive <sam5968947@yahoo.com.tw>
2023-01-02 15:45:36 +0800

    新增注釋

--- Array & Hashing/1. 两数之和/1. 两数之和.py
+++ Array & Hashing/1. 两数之和/1. 两数之和.py
@@ -4,6 +4,6 @@ class Solution:
         for i, v in enumerate(nums):
             if v in dic:
                 return [dic[v], i]
-            temp = target - v#補數
+            temp = target - v#補數為key, index為value。後續若有數等於補數時即找到答案
             dic[temp] = i
         return []
