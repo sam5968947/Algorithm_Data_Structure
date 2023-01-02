ca0e5038ce20478291343dbceb51ed9622a16623
SamisAlive <sam5968947@yahoo.com.tw>
2023-01-02 15:30:32 +0800

    新增注釋

--- Trees/100. 相同的树/100. 相同的树.py
+++ Trees/100. 相同的树/100. 相同的树.py
@@ -7,5 +7,5 @@
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         if not p and not q:return True
-        if not p or not q:return False
+        if not p or not q:return False #若有1空1非空，False。若都非空繼續比
         return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
