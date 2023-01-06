f132cd1ecdabd7b8e6fba7156dcb767e87b250ee
SamisAlive <sam5968947@yahoo.com.tw>
2023-01-06 17:32:03 +0800

    first commit

--- BitManipulation/190. 颠倒二进制位/190. 颠倒二进制位.cpp
+++ BitManipulation/190. 颠倒二进制位/190. 颠倒二进制位.cpp
@@ -4,7 +4,9 @@ public:
         uint32_t res = 0;
 
         for(int i = 0; i < 32; i++){
+            // 取最左數
             uint32_t bit = 1 & (n >> i);
+            // bit由左往右推(31-i)位
             res += bit << (31 - i);
         }
 
